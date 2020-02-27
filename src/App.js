a63545f48c35a3aab05c74b98e1b90856a7be98b
fragilejunkie <seuilblanc@gmail.com>
2020-02-26 21:05:19 -0300

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  <div id="content">
+    
+    <ul id="board">
+      
+      <div class="row">
+
+        <li class="position1"> <img src="../img/pawn.svg" alt="Autochess Piece" /> </li>
+        <li class="position2"></li>
+        <li class="position3"></li>
+        <li class="position4"></li>
+        <li class="position5"></li>
+        <li class="position6"></li>
+        <li class="position7"></li>
+        <li class="position8"></li>
+
+      </div>
+
+      <div class="row">
+
+        <li class="position9"></li>
+        <li class="position10"></li>
+        <li class="position11"></li>
+        <li class="position12"></li>
+        <li class="position13"></li>
+        <li class="position14"></li>
+        <li class="position15"></li>
+        <li class="position16"></li>
+      
+      </div>
+
+      <div class="row">
+
+        <li class="position17"></li>
+        <li class="position18"></li>
+        <li class="position19"></li>
+        <li class="position20"></li>
+        <li class="position21"></li>
+        <li class="position22"></li>
+        <li class="position23"></li>
+        <li class="position24"></li>
+      
+      </div>
+
+      <div class="row">
+
+        <li class="position25"></li>
+        <li class="position26"></li>
+        <li class="position27"></li>
+        <li class="position28"></li>
+        <li class="position29"></li>
+        <li class="position30"></li>
+        <li class="position31"></li>
+        <li class="position32"></li>
+      
+      </div>
+
+      <div class="row">
+
+        <li class="position33"></li>
+        <li class="position34"></li>
+        <li class="position35"></li>
+        <li class="position36"></li>
+        <li class="position37"></li>
+        <li class="position38"></li>
+        <li class="position39"></li>
+        <li class="position40"></li>
+      
+      </div>
+
+      <div class="row">
+
+        <li class="position41"></li>
+        <li class="position42"></li>
+        <li class="position43"></li>
+        <li class="position44"></li>
+        <li class="position45"></li>
+        <li class="position46"></li>
+        <li class="position47"></li>
+        <li class="position48"></li>
+      
+      </div>
+
+      <div class="row">
+
+        <li class="position49"></li>
+        <li class="position50"></li>
+        <li class="position51"></li>
+        <li class="position52"></li>
+        <li class="position53"></li>
+        <li class="position54"></li>
+        <li class="position55"></li>
+        <li class="position56"></li>
+      
+      </div>
+
+      <div class="row">
+
+        <li class="position57"></li>
+        <li class="position58"></li>
+        <li class="position59"></li>
+        <li class="position60"></li>
+        <li class="position61"></li>
+        <li class="position62"></li>
+        <li class="position63"></li>
+        <li class="position64"></li>
+      
+      </div>
+
+    </ul>
+
+  </div>
+
   );
 }
 
